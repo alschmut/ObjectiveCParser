39a0249d223f9799cdb291bd4f6d1b2055d44a13
Alexander Schmutz <aschmutz1@web.de>
2024-03-08 16:53:51 +0100

    Add tests

--- src/test/ProjectModel.test.py
+++ src/test/ProjectModel.test.py
@@ -1,25 +1,19 @@
-import sys, os, unittest, warnings
+import sys, os, unittest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../main'))
 
 from model.ProjectModel import ProjectModel
+from util.FileOpener import FileOpener
 
 class ProjectModelTest(unittest.TestCase):
 
-    path = "./ExampleJavaClass.java"
-    project_name = "ExampleJavaClass"
-    project_output_file = project_name + ".json"
+    path = "ExampleMethods.m"
     project_model: ProjectModel = None
 
     @classmethod
     def setUpClass(cls):
-        cls.ignore_numpy_warning()
         cls.project_model = ProjectModel(cls.path)
         cls.project_model.parse_file()
-
-    @classmethod
-    def ignore_numpy_warning(cls):
-        warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
     def test_project_has_1_file(self):
         self.assertEqual(len(self.project_model.files), 1)
@@ -28,21 +22,19 @@ class ProjectModelTest(unittest.TestCase):
         file = self.project_model.files[0]
         self.assertEqual(file.path, self.path)
 
-    def test_file_has_9_dictionary_entries(self):
-        dictionary = self.project_model.files[0].identifier_dictionary_model.dictionary
-        self.assertEqual(len(dictionary), 9)
+    def test_file_has_analysed_correct_number_of_methods(self):
+        methods = self.project_model.files[0].methods
+        self.assertEqual(len(methods), 24)
 
-    def test_dictionary_contains_correct_class_name(self):
-        dictionary = self.project_model.files[0].identifier_dictionary_model.dictionary
-        class_identifier = dictionary.get(self.project_name)
-        self.assertEqual(class_identifier.frequency, 1)
-        self.assertEqual(len(class_identifier.separated_words), 3)
+    def test_first_analysed_method_has_correct_name_and_correct_metric(self):
+        first_method = self.project_model.files[0].methods[0]
+        self.assertEqual(first_method.name, "initialize")
+        self.assertEqual(first_method.metrics().get("method_loc"), 5)
 
-    def test_dictionary_contains_correct_foo_identifier(self):
-        dictionary = self.project_model.files[0].identifier_dictionary_model.dictionary
-        foo_identifier = dictionary.get("foo")
-        self.assertEqual(foo_identifier.frequency, 2)
-        self.assertEqual(len(foo_identifier.separated_words), 1)
+    def test_creates_correct_csv(self):
+        csv = self.project_model.to_csv()
+        expected_csv = FileOpener().get_file_content("ExpectedExampleMethodsResult.csv")
+        self.assertEqual(csv, expected_csv)
 
 
 if __name__ == '__main__':
